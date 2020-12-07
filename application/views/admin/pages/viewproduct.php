9d2fc6c88b92ab8b9ed95686c50c934e3d9cf6b9
Amir Sahil <48803384+AmirSahil@users.noreply.github.com>
2020-12-08 00:17:46 +0530

    view product

--- application/views/admin/pages/viewproduct.php
+++ application/views/admin/pages/viewproduct.php
@@ -7,70 +7,19 @@
     </div>
     <?php extract($edit);?>
     <div class="d-sm-flex align-items-center justify-content-between mb-2 pl-4">
-        <h1 class="h3 mb-0 text-gray-800">Product ID : <?php echo $id;?></h1>
+        <h1 class="pid">Product ID : <?php echo $id;?></h1>
     </div>
     
-    <form method="post" class="pl-5" action="<?php echo base_url()?>Admin/update/<?php echo $id;?>">
-        <div class="form-group row pr-5">
-            <?php
-                if($this->uri->segment(3) == "inserted"){
-                    echo '<p class="text-success">Data Inserted</p>';
-                }
-                if($this->uri->segment(2) == "updated")  
-                {  
-                echo '<p class="text-success">Data Updated</p>';  
-                }  
-            ?>  
-        </div>
-        <div class="form-group row pr-5">
-            <div class="col-xs-6">
-                <label for="pname">Product Name</label>
-                <input type="text" class="form-control" placeholder="Product Name" value="<?php echo $product_name;?>" name="pname">
-                <span class="text-danger"><?php echo form_error("pname");?></span>
+    <div class="products">
+        <div class="tiles">  
+            <div class="image">
+                <img class="pimage" src="<?php echo base_url()?>uploads/pic<?php echo $id; ?>.png" alt="">
+            </div>
+            <div class="details">
+                <h3 class="pname"><?php echo $product_name; ?></h3>
+                <h4 class="pprice">₹<?php echo $product_price; ?>/-</h4>
+                <h4 class="pqty">Quantity Available: &nbsp;<?php echo $product_qty;?></h4>
+                <span class="pcategory"><?php echo $product_category; ?></span>
             </div>
         </div>
-        <div class="form-group row pr-5">
-            <div class="col-xs-6">
-                <label for="pprice">Product Price</label>
-                <input type="text" class="form-control" placeholder="Product Price" value="<?php echo $product_price;?>" name="pprice">
-                <span class="text-danger"><?php echo form_error("pprice");?></span>
-            </div>
-        </div>
-        <div class="form-group row pr-5">
-            <div class="col-xs-6">
-                <label for="pqty">Product Qty</label>
-                <input type="text" class="form-control" placeholder="Enter Quantity" value="<?php echo $product_qty;?>" name="pqty">
-                <span class="text-danger"><?php echo form_error("pqty");?></span>
-            </div>
-        </div>
-        <div class="form-group row pr-5">
-            <div class="col-xs-6">
-                <label for="pimage">Product Image</label>
-                <input type="text" class="form-control" placeholder="Enter Image" value="<?php echo $product_image;?>" name="pimage">
-            </div>
-        </div>
-        <!-- <div class="form-group row pr-5">
-            <div class="col-xs-6">
-                <label for="pcategory">Product Category</label>
-                <input type="text" class="form-control" placeholder="Enter Category" name="pcategory">
-            </div>
-        </div> -->
-        <div class="form-group row pr-5">
-            <div class="col-xs-6">
-                <label for="pcategory">Product Category</label>
-                <select class="form-control" <?php echo $product_category;?> name="pcategory">
-                    <option>Shirt</option>
-                    <option>Pant</option>
-                    <option>Shorts</option>
-                    <option>T-Shirt</option>
-                    <option>Shoes</option>
-                    <option>Socks</option>
-                    <option>Others</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group row pr-5 pt-2">
-            <div class="col-xs-6">
-                <input type="submit" class="btn btn-dark btn-md" name="upload" value="Edit"/>
-            </div>
-        </div>
+    </div>
